3e2e871d28ea42ff5a86262c8516d6ed4a781ced
Gabriel Alves <99101659+Alvezin@users.noreply.github.com>
2022-08-18 11:48:06 -0300

    configuração do servidor e inicio da criação de rotas

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,23 @@
-import express from 'express'
+import express, {Request, Response} from 'express'
 import dotenv from 'dotenv'
-import mustacheExpress from 'mustache-express'
+import mustache from 'mustache-express'
+import path from 'path'
+
+import mainRoutes from './routes/index'
 
 const server = express()
 dotenv.config()
 
 server.set('view engine', 'mustache')
-server.set('engine', mustacheExpress())
+server.set('views', path.join(__dirname, 'views'))
+server.set('engine', mustache())
 
+server.use(express.static(path.join(__dirname, '../public')))
 
+server.use(mainRoutes)
+//rotas
 
-
+server.use((req:Request, res:Response) => {
+    res.send('Página não encontrada')
+})
 server.listen(process.env.PORT)
